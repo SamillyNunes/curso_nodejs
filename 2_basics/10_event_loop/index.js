98d2869d57780bca919a6c53c06a46ae436039c2
Samilly <samillynunes19@gmail.com>
2024-08-26 19:57:13 -0300

    basics: working with events

---
+++ 2_basics/10_event_loop/index.js
@@ -0,0 +1,14 @@
+function a(){
+    console.log("Executando a()");
+}
+function b(){
+    console.log("Executando b()");
+}
+
+function c(){
+    console.log("Executando c()");
+    a();
+    b();
+}
+
+c();
