43d558d8f6c62e505d067f00eada8519011bfc27
Samilly <samillynunes19@gmail.com>
2024-08-27 11:57:10 -0300

    basics: reading and presenting html file

---
+++ 3_core_modules/5_fs/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+
+const server = http.createServer((req, res)=> {
+    fs.readFile('index.html', (err, data)=> {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+    
+});
+
+server.listen(port, ()=>{
+    console.log(`Servidor rodando na porta: ${port}`);
+});
